1f4929cadeab770bd294940105f23f40b67a59c6
masatoshi567 <isptry5689@gmail.com>
2020-02-09 16:11:22 +0900

    ログイン認証の実装

--- demo/src/main/java/com/example/demo/config/MvcConfig.java
+++ demo/src/main/java/com/example/demo/config/MvcConfig.java
@@ -6,11 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/home").setViewName("home");
-		registry.addViewController("/").setViewName("home");
-		registry.addViewController("/hello").setViewName("hello");
-		registry.addViewController("/login").setViewName("login");
-	}
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/top").setViewName("top");
+        registry.addViewController("/login").setViewName("login");
+    }
 }

7a7484c64c93921124f83ccf2791a24e17411aa9
haku10 <isptry5689@gmail.com>
2020-02-16 23:01:48 +0900

    KTSpringBootExam add

---
+++ KtSpringBootExam/src/main/kotlin/com/example/examkt/ExamktApplication.kt
@@ -0,0 +1,11 @@
+package com.example.examkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExamktApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExamktApplication>(*args)
+}
---
+++ KtSpringBootExam/src/test/kotlin/com/example/examkt/ExamktApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.examkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExamktApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
